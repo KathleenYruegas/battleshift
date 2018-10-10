9c6518f2dbbdd4ce0965cb2245e16bfe5a3f3de7
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-10 11:58:56 -0600

    Adds feature test for guest to register

---
+++ spec/features/guest_can_register_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+# As a guest user
+feature 'As a guest user' do
+  describe "when I visit /" do
+    scenario 'I can register' do
+# When I visit "/"
+      visit '/'
+# And I click "Register"
+      click_on "Register"
+# Then I should be on "/register"
+      expect(current_path).to eq('/register')
+# And when I fill in an email address (required)
+# And I fill in name (required)
+# And I fill in password and password confirmation (required)
+      fill_in :email, with: "myemail@email.com"
+      fill_in :name, with: "Sample Person"
+      fill_in :password, with: "test"
+      fill_in :password_confirmation, with: "test"
+# And I click submit
+      click_on "Submit"
+# Then I should be redirected to "/dashboard"
+# And I should see a message that says "Logged in as <SOME_NAME>"
+# And I should see "This account has not yet been activated. Please check your email."
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content("Logged in as Sample Person.")
+      expect(page).to have_content("This account has not yet been activated. Please check your email.")
+    end
+  end
+end
