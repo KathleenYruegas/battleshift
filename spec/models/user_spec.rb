2a948e06d3ea9de536bea04223d32defef4a90b3
ClaireBvs <clairembeauvais@gmail.com>
2018-10-10 17:23:44 -0600

    add model test user has default status to non active

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,12 @@ describe User do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe 'attributes' do
+    it 'should have default status as non-active' do
+      user = User.create(name: 'Elena', email: 'elena@example.com', password: 'test')
+
+      expect(user.status).to eq('non-active')
+    end
+  end
 end
