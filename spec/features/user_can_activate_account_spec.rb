7575b6d57899bc5eeb5285b13b849c46b41e519a
ClaireBvs <clairembeauvais@gmail.com>
2018-10-10 17:18:23 -0600

    add feature test user activate their account:

---
+++ spec/features/user_can_activate_account_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'guest user can activate their account' do
+  scenario 'when clicking email link' do
+    user = create(:user, status: 'non-active')
+
+    visit '/confirmation'
+
+    expect(user.status).to eq('active')
+
+    visit '/dashboard'
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("Status: Active")
+  end
+end
