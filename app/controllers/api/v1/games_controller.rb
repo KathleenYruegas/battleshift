d501e7b96d2f23385570cbb8f2f1429dd8e495f7
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-12 17:18:30 -0600

    Adds create action to game controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -6,8 +6,16 @@ module Api
         if game
          render json: game
         else
-         render status: 400 
+         render status: 400
        end
+      end
+
+      def create
+        player_1_board = Board.new(10)
+        player_2_board = Board.new(10)
+        player_1 = Player.new(player_1_board)
+        player_2 = Player.new(player_2_board)
+        render json: Game.create(player_1_board: player_1_board, player_2_board: player_2_board)
       end
     end
   end
