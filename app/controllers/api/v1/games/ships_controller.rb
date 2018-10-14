52dd2e5ca3686b472fd539a44b3e08f984ad414e
ClaireBvs <clairembeauvais@gmail.com>
2018-10-14 14:12:04 -0600

    wip on create method for ShipsController and private #ship_attributes for ShipPlacer

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,4 +1,24 @@
 class Api::V1::Games::ShipsController < ApplicationController
   def create
+    @game ||= Game.find(params[:game_id])
+
+    player_1_ships = ShipPlacer.new(ship_attributes)
+    player_1_ships.run
+    # require "pry"; binding.pry
+
+    # player_2_ships = ShipPlacer.new(ship_attributes)
+    # player_2_ships.run
+    @game.save
+    render json: @game, message: player_1_ships.message
+  end
+
+  private
+
+  def ship_attributes
+    { board: @game.player_1_board,
+      ship: Ship.new(params[:ship_size]),
+      start_space: params[:start_space],
+      end_space: params[:end_space]
+    }
   end
 end
