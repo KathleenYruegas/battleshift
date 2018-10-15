fb8341b86c03e829cba21984b920b3ffe2989cdd
ClaireBvs <clairembeauvais@gmail.com>
2018-10-14 18:14:21 -0600

    add requests test for POST api/v1/games

--- spec/requests/user_can_see_a_game_spec.rb
+++ spec/requests/user_can_see_a_game_spec.rb
@@ -37,7 +37,7 @@ describe 'GET /api/v1/games/1' do
                       player_2_board: player_2_board,
                       player_1_turns: 0,
                       player_2_turns: 0,
-                      current_turn: "challenger"
+                      current_turn: "player_1"
                     }
 
       game = Game.new(game_attributes)
@@ -65,6 +65,65 @@ describe 'GET /api/v1/games/1' do
       get "/api/v1/games/1"
 
       expect(response.status).to be(400)
+    end
+  end
+
+  describe 'POST api/v1/games' do
+    it 'returns a game with boards' do
+      player_1_board = Board.new(4)
+      player_2_board = Board.new(4)
+      sm_ship = Ship.new(2)
+      md_ship = Ship.new(3)
+
+      ShipPlacer.new(board: player_1_board,
+                     ship: sm_ship,
+                     start_space: "A1",
+                     end_space: "A2"
+                    ).run
+
+      ShipPlacer.new(board: player_1_board,
+                     ship: md_ship,
+                     start_space: "B1",
+                     end_space: "D1"
+                    ).run
+
+      ShipPlacer.new(board: player_2_board,
+                     ship: sm_ship.dup,
+                     start_space: "A1",
+                     end_space: "A2"
+                    ).run
+
+      ShipPlacer.new(board: player_2_board,
+                     ship: md_ship.dup,
+                     start_space: "B1",
+                     end_space: "D1"
+                    ).run
+
+      game_attributes = {
+                      player_1_board: player_1_board,
+                      player_2_board: player_2_board,
+                      player_1_turns: 0,
+                      player_2_turns: 0,
+                      current_turn: "player_1"
+                    }
+
+      game = Game.new(game_attributes)
+      game.save!
+
+      post "/api/v1/games"
+
+      actual  = JSON.parse(response.body, symbolize_names: true)
+      expected = Game.last
+
+      expect(response).to be_success
+      expect(actual[:id]).to eq(expected.id)
+      expect(actual[:current_turn]).to eq(expected.current_turn)
+      expect(actual[:player_1_board][:rows].count).to eq(4)
+      expect(actual[:player_2_board][:rows].count).to eq(4)
+      expect(actual[:player_1_board][:rows][0][:name]).to eq("row_a")
+      expect(actual[:player_1_board][:rows][3][:data][0][:coordinates]).to eq("D1")
+      expect(actual[:player_1_board][:rows][3][:data][0][:coordinates]).to eq("D1")
+      expect(actual[:player_1_board][:rows][3][:data][0][:status]).to eq("Not Attacked")
     end
   end
 end
