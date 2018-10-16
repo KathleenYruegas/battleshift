41bdda28ade13fb2725f55d01be2f3879e53000f
ClaireBvs <clairembeauvais@gmail.com>
2018-10-16 11:57:19 -0600

    add #correct_player? and render 400 if invalid turn

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -5,11 +5,15 @@ module Api
         def create
           game = Game.find(params[:game_id])
 
-          shooting_player = User.find_by_api_key(request.env["HTTP_X_API_KEY"])
+          shooting_player = User.find_by_api_key(request.headers["X-API-Key"])
           turn_processor = TurnProcessor.new(game, params[:shot][:target], shooting_player)
 
-          turn_processor.run!
-          render json: game, message: turn_processor.message
+          if turn_processor.correct_player?
+            turn_processor.run!
+            render json: game, message: turn_processor.message
+          else
+            render json: game, status: 400, message: turn_processor.message
+          end
         end
       end
     end
--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -8,8 +8,7 @@ class TurnProcessor
 
   def run!
     begin
-      # correct_player?
-      attack if correct_player?
+      attack
       game.save!
     rescue InvalidAttack => e
       @messages << e.message
@@ -20,13 +19,22 @@ class TurnProcessor
     @messages.join(" ")
   end
 
+  def correct_player?
+    if (game.current_turn == 'player_1') && (shooting_player == game.player_2)
+      @messages << "Invalid move. It's your opponent's turn"
+      return false
+    elsif (game.current_turn == 'player_2') && (shooting_player == game.player_1)
+      @messages << "Invalid move. It's your opponent's turn"
+      return false
+    else
+      return true
+    end
+  end
+
   private
 
-  attr_reader :game, :target
+  attr_reader :game, :target, :shooting_player
 
-  def correct_player?
-    @messages << "Invalid move. It's your opponent's turn"
-  end
 
   def attack
     if game.current_turn == 'player_1'
