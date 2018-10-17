fd4ee52bb731a75ed698cdfa13b257f345f47bde
ClaireBvs <clairembeauvais@gmail.com>
2018-10-17 16:33:34 -0600

    add logic to prevent not authorized player to play

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::Games::ShotsController < ApiController
     game = Game.find(params[:game_id])
 
     shooting_player = User.find_by_api_key(request.headers["X-API-Key"])
-    if shooting_player
+
+    if shooting_player && (shooting_player == game.player_1 || shooting_player == game.player_2)
       turn_processor = TurnProcessor.new(game, params[:shot][:target], shooting_player)
       if turn_processor.correct_player? && turn_processor.valid_spot? && turn_processor.no_winner?
         turn_processor.run!
