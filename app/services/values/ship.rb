eb1081e9475fd1190b5563ffd836bdb930bbb9b3
ClaireBvs <clairembeauvais@gmail.com>
2018-10-17 16:58:03 -0600

    remove comment refactor shots spec test

--- app/services/values/ship.rb
+++ app/services/values/ship.rb
@@ -1,19 +1,10 @@
 class Ship
   attr_reader :length, :damage
-              # :start_space,
-              # :end_space
 
   def initialize(length)
     @length = length
     @damage = 0
-    # @start_space = nil
-    # @end_space = nil
   end
-
-  # def place(start_space, end_space)
-  #   @start_space = start_space
-  #   @end_space = end_space
-  # end
 
   def hit!
     @damage += 1
