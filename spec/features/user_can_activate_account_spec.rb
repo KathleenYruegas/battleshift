b160b8a770abe797c701754ed9d4b2ece3e32781
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-18 14:28:23 -0600

    Fixes test to ensure activated user sees correct content

--- spec/features/user_can_activate_account_spec.rb
+++ spec/features/user_can_activate_account_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 feature 'guest user can activate their account' do
-  xscenario 'when clicking email link' do
+  scenario 'when clicking email link' do
     user = create(:user)
 
     visit edit_confirmation_path(user.activation_token)
 
     expect(page).to have_content("Thank you! Your account is now activated.")
-
-    # expect(user.status).to eq('active')
-
+    
     visit '/dashboard'
     expect(current_path).to eq('/dashboard')
     expect(page).to have_content("Status: Active")
